7bae74134f4ed5c63553e38dae8ad121033f3f50
Henrik Hallberg <halhen@k2h.se>
2009-04-23 22:49:11 +0200

    Unit tests reverted to use root node in the top level directory.

--- test.sh
+++ test.sh
@@ -7,29 +7,29 @@ mkdir -p mnt/
 
 # Do basic tests
 cd mnt/
-if [ `find ./ -name '*.node' | wc -l` -ne 2 ]; then
-    echo "Too many %d.nodes in .";
+if [ `find 1.root/ -name '*.node' | wc -l` -ne 2 ]; then
+    echo "Too many %d.nodes in 1.root";
 fi
 
-if [ `find ./1.node/ -name 'attribute' | wc -l` -ne 1 ]; then
-    echo "attribute error in ./1.node/";
+if [ `find 1.root/1.node/ -name 'attribute' | wc -l` -ne 1 ]; then
+    echo "attribute error in 1.root/1.node/";
 fi
-if [ `find ./1.node/ -name 'attribute2' | wc -l` -ne 0 ]; then
-    echo "attribute2 should not be in ./1.node";
+if [ `find 1.root/1.node/ -name 'attribute2' | wc -l` -ne 0 ]; then
+    echo "attribute2 should not be in 1.root/1.node";
 fi
-if [ `cat ./1.node/attribute` != "attrib_value" ]; then
-    echo "./1.node/attribute does not contain 'attrib_value'";
-fi
-
-if [ `find ./2.node/ -name 'attribute' | wc -l` -ne 0 ]; then
-    echo "attribute should not be in ./1.node/";
-fi
-if [ `find ./2.node/ -name 'attribute2' | wc -l` -ne 1 ]; then
-    echo "attribute2 error in ./1.node";
+if [ `cat 1.root/1.node/attribute` != "attrib_value" ]; then
+    echo "1.root/1.node/attribute does not contain 'attrib_value'";
 fi
 
-if [ "`cat ./1.textcontent/#text`" != "some text" ]; then
-    echo "Failed to read ./1.textcontent/#text properly";
+if [ `find 1.root/2.node/ -name 'attribute' | wc -l` -ne 0 ]; then
+    echo "attribute should not be in 1.root/1.node/";
+fi
+if [ `find 1.root/2.node/ -name 'attribute2' | wc -l` -ne 1 ]; then
+    echo "attribute2 error in 1.root/1.node";
+fi
+
+if [ "`cat 1.root/1.textcontent/#text`" != "some text" ]; then
+    echo "Failed to read 1.root/1.textcontent/#text properly";
 fi
 
 cd ..
